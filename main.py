fdebfa5d9409cf988cd92e6e4a1029f94e55d698
sadimfinity <santi-skate@hotmail.com>
2020-04-10 23:01:58 -0500

    Changing the & for and .-.

--- main.py
+++ main.py
@@ -6,146 +6,145 @@ import pygame
 current_path = os.getcwd()
 import pymunk as pm
 
-def drawScenario(screen):
-    screen.fill(white)
-    pygame.draw.rect(screen, black, [0,0,398,398])
+def drawScenario(screen, colors):
+    screen.fill(colors['white'])
+    pygame.draw.rect(screen, colors['black'], [0,0,398,398])
     #Draw start
-    pygame.draw.rect(screen, green, [-2+40*3,-2+40*9,40,40])
-    pygame.draw.rect(screen, green, [-2+40*4,-2+40*9,40,40])
+    pygame.draw.rect(screen, colors['green'], [-2+40*3,-2+40*9,40,40])
+    pygame.draw.rect(screen, colors['green'], [-2+40*4,-2+40*9,40,40])
     #Draw scenario
-    pygame.draw.rect(screen, yellow, [-2+40*3,-2+40*8,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*4,-2+40*8,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*2,-2+40*8,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*1,-2+40*8,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*1,-2+40*7,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*1,-2+40*6,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*1,-2+40*5,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*2,-2+40*5,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*3,-2+40*5,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*3,-2+40*6,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*4,-2+40*5,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*4,-2+40*6,40,40])
-    pygame.draw.rect(screen, blue, [-2+40*5,-2+40*6,40,40]) #Cuadrito de aumento de tamaño
-    pygame.draw.rect(screen, yellow, [-2+40*6,-2+40*6,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*6,-2+40*7,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*6,-2+40*8,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*7,-2+40*8,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*8,-2+40*8,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*8,-2+40*7,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*8,-2+40*6,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*8,-2+40*5,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*8,-2+40*4,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*7,-2+40*4,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*6,-2+40*4,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*5,-2+40*4,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*5,-2+40*3,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*4,-2+40*3,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*3,-2+40*3,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*2,-2+40*3,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*1,-2+40*3,40,40])
-    pygame.draw.rect(screen, blue, [-2+40*1,-2+40*2,40,40]) #Cuadritos de aumento de tamaño
-    pygame.draw.rect(screen, yellow, [-2+40*1,-2+40*1,40,40]) 
-    pygame.draw.rect(screen, yellow, [-2+40*2,-2+40*1,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*3,-2+40*1,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*4,-2+40*1,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*5,-2+40*1,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*6,-2+40*1,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*7,-2+40*1,40,40])
-    pygame.draw.rect(screen, yellow, [-2+40*8,-2+40*1,40,40])
-    pygame.draw.rect(screen, red, [-2+40*8,-2+40*0,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*3,-2+40*8,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*4,-2+40*8,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*2,-2+40*8,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*1,-2+40*8,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*1,-2+40*7,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*1,-2+40*6,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*1,-2+40*5,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*2,-2+40*5,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*3,-2+40*5,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*3,-2+40*6,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*4,-2+40*5,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*4,-2+40*6,40,40])
+    pygame.draw.rect(screen, colors['blue'], [-2+40*5,-2+40*6,40,40]) #Cuadrito de aumento de tamaño
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*6,-2+40*6,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*6,-2+40*7,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*6,-2+40*8,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*7,-2+40*8,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*8,-2+40*8,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*8,-2+40*7,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*8,-2+40*6,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*8,-2+40*5,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*8,-2+40*4,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*7,-2+40*4,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*6,-2+40*4,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*5,-2+40*4,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*5,-2+40*3,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*4,-2+40*3,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*3,-2+40*3,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*2,-2+40*3,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*1,-2+40*3,40,40])
+    pygame.draw.rect(screen, colors['blue'], [-2+40*1,-2+40*2,40,40]) #Cuadritos de aumento de tamaño
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*1,-2+40*1,40,40]) 
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*2,-2+40*1,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*3,-2+40*1,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*4,-2+40*1,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*5,-2+40*1,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*6,-2+40*1,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*7,-2+40*1,40,40])
+    pygame.draw.rect(screen, colors['yellow'], [-2+40*8,-2+40*1,40,40])
+    pygame.draw.rect(screen, colors['red'], [-2+40*8,-2+40*0,40,40])
     
 def isAValidMove(x, y, size, win, canPlay):
     if(not win):
-        if(isInTheStart(x, y, size, canPlay)):
-            if(x-size>40*3-2 & x+size<40*5-2 & y-size>40*8-2 &y+size<40*10-2):
+        canPlay = isInTheStart(x, y, size)
+        if(canPlay):
+            if(x-size>40*3-2 and x+size<40*5-2 and y-size>40*8-2 and y+size<40*10-2):
                   return True
-            if(x-size>40*3-2 & x+size<40*5-2 & y-size>40*8-2 &y+size<40*10-2):
+            if(x-size>40*3-2 and x+size<40*5-2 and y-size>40*8-2 and y+size<40*10-2):
                   return True
-            if(x-size>40*1-2 & x+size<40*5-2 & y-size>40*8-2 & y+size<40*9-2):
+            if(x-size>40*1-2 and x+size<40*5-2 and y-size>40*8-2 and y+size<40*9-2):
                   return True
-            if(x-size>40*1-2 & x+size<40*2-2 & y-size>40*5-2 & y+size<40*9-2):
+            if(x-size>40*1-2 and x+size<40*2-2 and y-size>40*5-2 and y+size<40*9-2):
                   return True
-            if(x-size>40*1-2 & x+size<40*5-2 & y-size>40*5-2 & y+size<=40*6-2):
+            if(x-size>40*1-2 and x+size<40*5-2 and y-size>40*5-2 and y+size<=40*6-2):
                   return True
-            if(x-size>40*3-2 & x+size<40*5-2 & y-size>40*5-2 & y+size<40*7-2):
+            if(x-size>40*3-2 and x+size<40*5-2 and y-size>40*5-2 and y+size<40*7-2):
                   return True
-            if(x-size>40*3-2 & x+size<40*7-2 & y-size>40*6-2 & y+size<=40*7-2):
+            if(x-size>40*3-2 and x+size<40*7-2 and y-size>40*6-2 and y+size<=40*7-2):
                   return True
-            if(x-size>40*6-2 & x+size<40*7-2 & y-size>40*6-2 & y+size<40*9-2):
+            if(x-size>40*6-2 and x+size<40*7-2 and y-size>40*6-2 and y+size<40*9-2):
                   return True
-            if(x-size>40*6-2 & x+size<40*9-2 & y-size>40*8-2 & y+size<40*9-2):
+            if(x-size>40*6-2 and x+size<40*9-2 and y-size>40*8-2 and y+size<40*9-2):
                   return True
-            if(x-size>=40*8-2 & x+size<=40*9-2 & y-size>=40*4-2 & y+size<=40*9-2):
+            if(x-size>=40*8-2 and x+size<=40*9-2 and y-size>=40*4-2 and y+size<=40*9-2):
                   return True
-            if(x-size>40*5-2 & x+size<=40*9-2 & y-size>=40*4-2 & y+size<40*5-2):
+            if(x-size>40*5-2 and x+size<=40*9-2 and y-size>=40*4-2 and y+size<40*5-2):
                   return True
-            if(x-size>40*5-2 & x<40*6-2 & y-size>40*3-2 & y+size<40*5-2):
+            if(x-size>40*5-2 and x<40*6-2 and y-size>40*3-2 and y+size<40*5-2):
                   return True
-            if(x-size>40*1-2 & x+size<40*6-2 & y-size>40*3-2 & y+size<=40*4-2):
+            if(x-size>40*1-2 and x+size<40*6-2 and y-size>40*3-2 and y+size<=40*4-2):
                   return True
-            if(x-size>40*1-2 & x+size<40*2-2 & y-size>40*1-2 & y+size<=40*4-2):
+            if(x-size>40*1-2 and x+size<40*2-2 and y-size>40*1-2 and y+size<=40*4-2):
                   return True
-            if(x-size>40*1-2 & x+size<=40*9-2 & y-size>=40*1-2 & y+size<=40*2-2):
+            if(x-size>40*1-2 and x+size<=40*9-2 and y-size>=40*1-2 and y+size<=40*2-2):
                   return True
-            if(x-size>40*8-2 & x+size<40*9-2 & y-size>40*0-2 & y+size<40*2-2):
+            if(x-size>40*8-2 and x+size<40*9-2 and y-size>40*0-2 and y+size<40*2-2):
                   return True
         else:
             canPlay = False
             return canPlay
             
 def didWin(x, y, size):
-    if(x-size>40*8-2 & x+size<40*9-2 & y-size>40*0-2 & y+size<40*1-2):
+    if(x>40*8-2 and x<40*9-2 and y>40*0-2 and y<40*1-2):
         return True
 
-def isInTheStart(x, y, size, canPlay):
-    if((x-size>40*3-2 & x+size<40*5-2 & y-size>40*9-2 & y+size<40*10-2) | canPlay):
+def isInTheStart(x, y, size):
+    if(x>40*3-2 and x<40*5-2 and y>40*9-2 and y<40*10-2):
         canPlay = True
     else:
         canPlay = False
+        
     return canPlay
 
 def increaseSize(x, y, size):
-    if(x-size>40*5-2 & x+size<40*6-2 & y-size>40*6-2 & y+size<40*7-2):
+    if(x>40*5-2 and x<40*6-2 and y>40*6-2 and y<40*7-2):
         return 1
-    elif(x-size>40*1-2 & x+size<40*2-2 & y-size>40*2-2 & y+size<40*3-2):
+    elif(x>40*1-2 and x<40*2-2 and y>40*2-2 and y<40*3-2):
         return 2
 
-def drawPlayer(x, y, screen, size):
-    pygame.draw.circle(screen, red, (x, y), size)
+def main():
+    size = 3
+    canPlay = False
+    win = False
+    white = (255, 255, 255)
+    black = (51,51,51)
+    red = (255,0,0)
+    green = (0,255,0)
+    yellow = (255,204,0)
+    blue = (0,0,255)
+    colors = dict(white = white, black = black, red = red, green = green, yellow = yellow, blue = blue)
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 650))
+    pygame.display.set_caption('The Maze')
+    gameExit = False
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-size = 3
-canPlay = False
-win = False
-white = (255, 255, 255)
-black = (51,51,51)
-red = (255,0,0)
-green = (0,255,0)
-yellow = (255,204,0)
-blue = (0,0,255)
+        drawScenario(screen, colors)
+        x, y = pygame.mouse.get_pos()
+        if(isAValidMove(x, y, size, win, canPlay)):
+            if(increaseSize(x, y, size) == 1):
+                size = size + 1
+            elif(increaseSize(x, y, size) == 2):
+                size = size + 2
+            pygame.draw.circle(screen, red, (x, y), size)
+            if(didWin(x, y, size)):
+                win = True
+        else:
+            size = 5
 
-pygame.init()
-screen = pygame.display.set_mode((1200, 650))
-pygame.display.set_caption('The Maze')
-#rect = pygame.Rect(181, 1050, 50, 50)
-#cropped = full_sprite.subsurface(rect).copy()
+        pygame.display.update()
 
-gameExit = False
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-    
-    drawScenario(screen)
-    x, y = pygame.mouse.get_pos()
-    if(isAValidMove(x, y, size, win, canPlay)):
-        if(increaseSize(x, y, size) == 1):
-            size = size + 1
-        elif(increaseSize(x, y, size) == 2):
-            size = size + 2
-        drawPlayer(x, y, screen, size)
-        if(didWin(x, y, size)):
-            win = True
-    else:
-        size = 5
-        
-    pygame.display.update()
+main()
